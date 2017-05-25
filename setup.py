8b23ce6556b4105da4080de51ed8a4501e853036
Oliver Bestwalter <oliver.bestwalter@avira.com>
2017-05-26 00:55:14 +0200

    prepare for pypi release

--- setup.py
+++ setup.py
@@ -2,17 +2,30 @@ from setuptools import find_packages, setup
 
 try:
     import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
+    longDescription = pypandoc.convert('README.md', 'rst')
 except(OSError, IOError, ImportError):
-    long_description = open('README.md').read()
+    longDescription = open('README.md').read()
 
-setup(
+kwargs = dict(
     name='i3configger',
     description="i3 config generation tool",
-    long_description=long_description,
+    long_description=longDescription,
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
     entry_points={'console_scripts': ['i3configger = i3configger.cli:main']},
     install_requires=['inotify', 'psutil', 'python-daemon'],
     packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: POSIX :: Linux',
+        'License :: OSI Approved :: MIT License',
+        'Environment :: Console',
+        'Topic :: Utilities',
+        'Topic :: Desktop Environment :: Window Managers :: i3wm',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.6',
+        ],
 )
+
+if __name__ == '__main__':
+    setup(**kwargs)
