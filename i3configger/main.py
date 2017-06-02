f1c91cb6388247ae6bfde6ce0033af70eb72ca80
Oliver Bestwalter <oliver.bestwalter@avira.com>
2017-06-03 00:22:28 +0200

    always process message if given

--- i3configger/main.py
+++ i3configger/main.py
@@ -14,16 +14,16 @@ def main():
         daemonize.exorcise()
         return 0
     configPath = paths.get_my_config_path(args.config)
+    if args.message:
+        p = paths.Paths(configPath)
+        config.State.process(p.state, partials.create(p.root), args.message)
     if daemonize.get_other_i3configgers():
         if not args.message:
             sys.exit("i3configger already running. "
                      "Did you want to send a message?")
         # process and write the state and let watcher do the build
         log.info("watcher process is running - processing message")
-        p = paths.Paths(configPath)
-        config.State.process(p.state, partials.create(p.root), args.message)
         return 0
-    # TODO merge args and conf (args override conf)
     ipc.I3.set_msg_type(args.i3_refresh_msg)
     log.info("set i3 refresh method to %s", ipc.I3.refresh)
     ipc.Notify.set_notify_command(args.no_notify)
@@ -45,12 +45,3 @@ def main():
         ipc.I3.refresh()
         ipc.StatusBar.refresh()
         ipc.Notify.send('new config active')
-
-
-if __name__ == '__main__':
-    # dev stuff
-    sys.argv = ['dev', '-vvv']
-    # sys.argv = ['dev', '-vvv', 'select-next', 'scheme']
-    # sys.argv = ['dev', '-vvv', 'set', 'mode', 'dock']
-    # sys.argv = ['dev', '-vvv', '--watch']
-    sys.exit(main())
