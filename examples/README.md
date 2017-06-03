3feb838e677938cbf265b782291c87015bcfa0a3
Oliver Bestwalter <oliver.bestwalter@avira.com>
2017-06-03 22:01:17 +0200

    examples and tests (using those examples)

--- examples/README.md
+++ examples/README.md
@@ -1,4 +1,25 @@
 # Examples
 
-**WARNING** Nothing (working) to see here yet. Still work in progress, will be part of the documentation later on ...
+## [default](0-default)
 
+The i3configger version of the i3 default configuration
+
+Taken from `/etc/i3/config`
+
+Simplest possible way of using it and a bit pointless, but you have to start somewhere.
+
+## [chunked](1-chunked)
+
+Different kinds of settings have their own files now and the bar get also build. The name fo the status bar config file is set dynamically with variables populated from `i3configger.json` bars->targets section for every bar. There is only one bar here though, so this doesn't really show yet.
+
+## [bars](2-bars)
+
+Let's generate more bars now. If you have an external monitor where you want to have a different bar, you can add another section to i3configger.json->bars->targets, add another status bar configuration file. Two bar settings will be generated in the config and the two configuration files are rendered and referenced from the variables in the bar template.
+
+## Variables
+
+**TODO**
+
+## Schemes
+
+**TODO**
--- tests/test_build.py
+++ tests/test_build.py
@@ -2,24 +2,37 @@ from pathlib import Path
 
 import pytest
 
-from i3configger import build, paths
+from i3configger import build, ipc, paths
 
 HERE = Path(__file__).parent
 EXAMPLES = HERE.parent / 'examples'
+REFERENCE = HERE / 'examples'
+FAKE_HEADER = """\
+###############################################################################
+# Built by i3configger /from/some/directory/who/cares  (some time after 1972) #
+###############################################################################
+"""
+TEST_FOLDER_NAMES = list(
+    [d.name for d in EXAMPLES.iterdir()
+     if d.is_dir() and not str(d.name).startswith('_')])
 
 
-@pytest.mark.parametrize(
-    "container", (
-        "default", "selectors"
-    ))
-def test_no_config(container, monkeypatch):
-    """Given empty sources directory a new config is created from defaults"""
-    pytest.skip("WIP")
-    container = EXAMPLES / container
-    monkeypatch.setattr(paths, 'get_i3_config_path', lambda: container)
+@pytest.mark.parametrize("container", TEST_FOLDER_NAMES)
+def test_no_build(container, monkeypatch):
+    ipc.Notify.set_notify_command(True)
+    monkeypatch.setattr(
+        paths, 'get_i3_config_path', lambda: EXAMPLES / container)
     configPath = paths.get_my_config_path()
     assert configPath.exists() and configPath.is_file()
-
     builder = build.Builder(configPath)
+    monkeypatch.setattr(builder, 'make_header', lambda: FAKE_HEADER)
     builder.build()
-    assert 0
+    buildPath = configPath.parents[1]
+    referencePath = REFERENCE / container
+    for path in referencePath.iterdir():
+        resultFilePath = buildPath / path.name
+        referenceFilePath = (REFERENCE / path.name)
+        assert resultFilePath != referenceFilePath
+        result = resultFilePath.read_text()
+        reference = resultFilePath.read_text()
+        assert result == reference
