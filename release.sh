143b1fc6e6ad75bce15daf21df61c80f5807498d
Oliver Bestwalter <oliver.bestwalter@avira.com>
2017-06-08 23:29:43 +0200

    still tuning that release process ...

--- release.sh
+++ release.sh
@@ -2,13 +2,19 @@
 
 set -e
 
+pip install pypandoc
+python i3configger/util.py
+git add docs/_pypi/*
+git commit -m "release $1"
+git push
+
 if [[ "$(git diff --shortstat 2> /dev/null | tail -n1)" != "" ]]; then
     echo "$(git diff)"
     echo "REPO IS DIRTY!"
     exit 1
 fi
 
-git tag $1
+git tag "$1"
 python setup.py sdist upload
 git push --tags
 echo "SUCCESS: released $(git describe --tags $(git rev-list --tags --max-count=1))"
