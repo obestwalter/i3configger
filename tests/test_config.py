5c5822dea388cb97b944ee45edc35387d10dd32f
Oliver Bestwalter <oliver.bestwalter@avira.com>
2018-05-11 11:54:19 +0200

    add an xfailing test to make sure this gets handled

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,5 +1,7 @@
 import json
 
+import pytest
+
 from i3configger import config
 
 
@@ -19,3 +21,21 @@ def test_initialization(tmpdir, monkeypatch):
     assert 'set' not in payload
     assert 'select' not in payload
     assert 'shadow' not in payload
+
+
+@pytest.mark.xfail(reason="test if already implemented, or implement it")
+def test_config_backup_is_not_overwritten():
+    """Going through the docs I saw this:
+
+    WARNING going ahead will overwrite your existing config file, so make
+    sure your config is under source control and/or you have a backup.
+    i3configger will create a backup of your old config but only one,
+    so running i3configger twice will leave no trace of your original
+    configuration file.
+
+    http://oliver.bestwalter.de/i3configger/getting-started/
+
+    I though I fixed that, but am not sure so this test should make sure that
+    the original config backup gets never overwritten.
+    """
+    assert 0  # FIXME
