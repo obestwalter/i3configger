d6879d6ca2f0b71202705204591effd22c822c73
Oliver Bestwalter <oliver.bestwalter@avira.com>
2018-12-17 20:13:31 +0100

    implement test to make sure backups are not overwritten

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,8 +1,8 @@
 import json
-
-import pytest
+from pathlib import Path
 
 from i3configger import config
+from i3configger.build import persist_results
 
 
 def test_initialization(tmpdir, monkeypatch):
@@ -23,19 +23,15 @@ def test_initialization(tmpdir, monkeypatch):
     assert "shadow" not in payload
 
 
-@pytest.mark.xfail(reason="test if already implemented, or implement it")
-def test_config_backup_is_not_overwritten():
-    """Going through the docs I saw this:
-
-    WARNING going ahead will overwrite your existing config file, so make
-    sure your config is under source control and/or you have a backup.
-    i3configger will create a backup of your old config but only one,
-    so running i3configger twice will leave no trace of your original
-    configuration file.
-
-    http://oliver.bestwalter.de/i3configger/getting-started/
-
-    I thought I fixed that, but am not sure so this test should make sure that
-    the original config backup gets never overwritten.
-    """
-    assert 0  # FIXME
+def test_config_backup_is_not_overwritten(tmp_path):
+    """Given an existing backup it is not overwritten by subsequent builds."""
+    firstThing = "first thing"
+    somePath = tmp_path / "some-path.txt"
+    somePath.write_text(firstThing)
+    persist_results({somePath: firstThing})
+    assert (Path(str(somePath) + ".bak")).read_text() == firstThing
+    somePath.write_text(firstThing)
+    otherThing = "other thing"
+    persist_results({somePath: otherThing})
+    assert somePath.read_text() == otherThing
+    assert (Path(str(somePath) + ".bak")).read_text() == firstThing
