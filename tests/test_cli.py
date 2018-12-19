fcc786c5e0680a700fd0270f6ac2a118d428774a
Oliver Bestwalter <oliver.bestwalter@avira.com>
2018-12-19 19:09:10 +0100

    helper to test the cli and a little test to get started

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -18,6 +18,7 @@ class Runner:
             cmd += args
         cp = subprocess.run(
             cmd,
+            timeout=1,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             cwd=otherCwd or self.cwd,
@@ -26,9 +27,10 @@ class Runner:
 
 
 @pytest.fixture(name="runner")
-def create_i3configger_runner(tmp_path, monkeypatch) -> Runner:
+def create_i3configger_runner(monkeypatch, tmp_path) -> Runner:
     with suppress(exc.UserError):
         watch.exorcise()
+    assert not watch.get_i3configger_process()
     monkeypatch.chdir(tmp_path)
     return Runner(tmp_path)
 
@@ -40,4 +42,29 @@ def test_help(runner):
     assert "usage:" in out
 
 
-# TODO add more tests for other commands
+def test_daemon(runner):
+    ret, out, err = runner(["--daemon"])
+    assert ret == 0
+    assert not err
+    assert not out
+    assert watch.get_i3configger_process()
+    watch.exorcise()
+    assert not watch.get_i3configger_process()
+
+
+def test_kill_with_running_i3configger_works(runner):
+    runner(["--daemon"])
+    assert watch.get_i3configger_process()
+    ret, out, err = runner(["--kill"])
+    assert ret == 0
+    assert not err
+    assert not out
+    assert not watch.get_i3configger_process()
+
+
+def test_kill_without_running_i3configger_gives_good_error(runner):
+    ret, out, err = runner(["--kill"])
+    assert ret == 1
+    assert "no daemon running - nothing to kill" in err
+    assert not out
+    assert not watch.get_i3configger_process()
