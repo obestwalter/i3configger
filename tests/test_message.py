69c72d6222fb96426b8080bd2fbe5ae719413c93
Oliver Bestwalter <oliver.bestwalter@avira.com>
2018-12-30 14:08:23 +0100

    black

--- tests/test_message.py
+++ tests/test_message.py
@@ -2,5 +2,5 @@ from i3configger.message import Messenger
 
 
 def test_shadow(tmp_path):
-    messenger = Messenger(tmp_path / "dontcare",  [], message=["shadow", "k", "v"])
+    messenger = Messenger(tmp_path / "dontcare", [], message=["shadow", "k", "v"])
     messenger.digest_message()
